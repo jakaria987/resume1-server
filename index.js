940cdf29f045fc101c02b7ca8fdf09f7bed343bb
Md Jakaria <jakhossain999@gmail.com>
2023-05-02 08:55:35 +0600

    load first data in UI

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 5000
+const chef = require('./data/chef.json');
 
 app.get('/', (req, res) => {
     res.send('chef data uploading');
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
 })
 
 app.listen(port, () => {
