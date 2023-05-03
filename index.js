39f2843ce58c963e71457e502c2c4a7278643f7e
Md Jakaria <jakhossain999@gmail.com>
2023-05-03 06:27:59 +0600

    update sigle chef details

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000
+const cors = require('cors');
 const chef = require('./data/chef.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('chef data uploading');
@@ -9,6 +12,11 @@ app.get('/', (req, res) => {
 
 app.get('/chef', (req, res) => {
     res.send(chef);
+})
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chef.find(c => c._id == id);
+    res.send(selectedChef);
 })
 
 app.listen(port, () => {
